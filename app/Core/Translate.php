f1506c745ca4da3749ba67bd3338e938377c6d7d
alexvidal <contato@webav.com.br>
2022-09-10 06:05:43 -0300

    bug fixed error return

--- app/Core/Translate.php
+++ app/Core/Translate.php
@@ -16,32 +16,38 @@ class Translate extends \Erykai\Translate\Translate
         unset($return->object()->message, $return->object()->nameDefault);
         return $return;
     }
+
     public function translator(object $response, string $nameDefault)
     {
         //validate return components ex response
-        if(isset($response->message)){
+        if (isset($response->message)) {
             $response->text = $response->message;
+            unset($response->message);
         }
-        if(!isset($response->text))
-        {
+        if (!isset($response->text)) {
             die('to use the Translate component send an object that contains the example attribute $data->text = "route";');
         }
         $return = new Response();
         $response->file = $nameDefault;
         $return->data($this->data($response)->target()->response());
         unset($return->object()->message, $return->object()->nameDefault);
+        if ($nameDefault === 'message') {
+            $response->text = $return->getResponse()->translate;
+            $return->data($response);
+        }
         return $return;
+
     }
 
     public function router(string $route, string $dynamic = null)
     {
         $routerTranslate = new stdClass();
-        if($dynamic){
+        if ($dynamic) {
             $routerTranslate->dynamic = $dynamic;
         }
         $routerTranslate->text = $route;
         $router = $this->translator($routerTranslate, "route")->object()->translate;
-        if(strpos($router, "<&>")){
+        if (strpos($router, "<&>")) {
             die($router . " in variable " . '$routeDefault and $routeTranslate');
         }
 
