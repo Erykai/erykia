b4e76ffe033f62dcd125b5cb044644da7f883c0f
alexvidal <contato@webav.com.br>
2023-03-31 06:51:54 -0300

    inverter

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 composer install --no-progress --no-interaction
+exec apache2-foreground
 cd /var/www/html/public/dashboard/assets
 npm install
 npm start
 cd ../../../
-exec apache2-foreground
--- public/default/index.php
+++ public/default/index.php
@@ -1,10 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>template frontend</title>
-</head>
-<body>
-<h1>template frontend</h1>
-</body>
-</html>
+<h1>HELLO</h1>
