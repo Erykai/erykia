ed7f5acdd11d471bb208a6a1afac6e044c4c950b
alexvidal <contato@webav.com.br>
2023-04-02 16:44:56 -0300

    ajust template dashboard

--- public/dashboard/assets/js/datatables/datatables-simple-demo.js
+++ public/dashboard/assets/js/datatables/datatables-simple-demo.js
@@ -1,8 +1,3 @@
-function wrapTextWithBraces(element) {
-    const textContent = element.textContent.trim();
-    element.textContent = "{{" + textContent + "}}";
-}
-
 window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
@@ -11,13 +6,4 @@ window.addEventListener('DOMContentLoaded', event => {
     if (datatablesSimple) {
         new simpleDatatables.DataTable(datatablesSimple);
     }
-
-    // Wrap specific texts with braces
-    const entriesPerPageLabel = document.querySelector('.datatable-dropdown label');
-    const searchInput = document.querySelector('.datatable-search input');
-    const datatableInfo = document.querySelector('.datatable-info');
-
-    wrapTextWithBraces(entriesPerPageLabel);
-    wrapTextWithBraces(datatableInfo);
-    searchInput.placeholder = "{{" + searchInput.placeholder + "}}";
 });
--- public/dashboard/pages/home.php
+++ public/dashboard/pages/home.php
@@ -18,7 +18,7 @@
 <!-- Main page content-->
 <div class="container-xl px-4 mt-n10">
     <div class="row">
-        <div class="col-xxl-4 col-xl-12 mb-4">
+        <div class="col-xl-12 mb-4">
             <div class="card h-100">
                 <div class="card-body h-100 p-5">
                     <div class="row align-items-center">
