488a7eda60df29963c98769b7d7243eb985c283e
alexvidal <contato@webav.com.br>
2023-04-17 17:32:05 -0300

    create curl auto install

--- routes/Module/Module.php
+++ routes/Module/Module.php
@@ -4,3 +4,12 @@ if (!isset($route)) {
 }
 $route->namespace('Source\Controller\Module');
 $route->default( '/module','ModuleController',[true, true, true, true], type: 'json', translate: false);
+
+//Route::get('bash', function(){
+//    $bash =  '#!/bin/bash'.PHP_EOL;
+//    $bash .= 'echo "Hello, Im Erykia"'.PHP_EOL;
+//    $bash .=  'git clone https://github.com/erykai/erykia.git bagual'.PHP_EOL;
+//    $bash .=  'cd bagual'.PHP_EOL;
+//    $bash .=  'docker-compose up -d --build'.PHP_EOL;
+//    return $bash;
+//});
