e0fee7da76012495c1437ff955b4ae124618d301
alexvidal <contato@webav.com.br>
2023-04-20 14:12:51 -0300

    fixed bug init

--- app/View/Ia/View.php
+++ app/View/Ia/View.php
@@ -8,7 +8,7 @@ class View extends Controller
 {
     private static $instance;
 
-    protected function __construct()
+    public function __construct()
     {
         parent::__construct();
     }
